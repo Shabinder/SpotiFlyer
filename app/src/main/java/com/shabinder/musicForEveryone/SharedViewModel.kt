1d88386f7aa3ce74306287a54d35e7c8061bcae8
shabinder <mr.shabinder@outlook.com>
2020-07-25 12:17:29 +0530

    Licence Notice Added.

--- app/src/main/java/com/shabinder/musicForEveryone/SharedViewModel.kt
+++ app/src/main/java/com/shabinder/musicForEveryone/SharedViewModel.kt
@@ -31,22 +31,7 @@ import com.shabinder.musicForEveryone.utils.SpotifyService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-/*
-* Copyright (C)  2020  Shabinder Singh
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+
 class SharedViewModel : ViewModel() {
     var intentString = ""
     var accessToken = MutableLiveData<String>().apply { value = "" }
--- app/src/main/java/com/shabinder/musicForEveryone/fragments/MainViewModel.kt
+++ app/src/main/java/com/shabinder/musicForEveryone/fragments/MainViewModel.kt
@@ -20,24 +20,6 @@ package com.shabinder.musicForEveryone.fragments
 import androidx.lifecycle.ViewModel
 import com.shabinder.musicForEveryone.models.Track
 
-/*
-* Copyright (C)  2020  Shabinder Singh
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
-
 class MainViewModel: ViewModel() {
     var searchLink:String = ""
     var trackList = mutableListOf<Track>()
