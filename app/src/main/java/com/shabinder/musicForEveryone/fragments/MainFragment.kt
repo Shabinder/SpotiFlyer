4642897d7eb4922a4f83daef3f4ee896c7cbe10f
shabinder <mr.shabinder@outlook.com>
2020-07-25 12:09:34 +0530

    Licence Added.

--- app/src/main/java/com/shabinder/musicForEveryone/fragments/MainFragment.kt
+++ app/src/main/java/com/shabinder/musicForEveryone/fragments/MainFragment.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C)  2020  Shabinder Singh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.shabinder.musicForEveryone.fragments
 
 import android.content.Context
@@ -27,8 +44,22 @@ import com.shabinder.musicForEveryone.utils.bindImage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-
-
+/*
+* Copyright (C)  2020  Shabinder Singh
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 @Suppress("DEPRECATION")
 class MainFragment : Fragment(),DownloadHelper {
     private lateinit var binding:MainFragmentBinding
--- app/src/main/java/com/shabinder/musicForEveryone/recyclerView/TrackListAdapter.kt
+++ app/src/main/java/com/shabinder/musicForEveryone/recyclerView/TrackListAdapter.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C)  2020  Shabinder Singh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.shabinder.musicForEveryone.recyclerView
 
 import android.view.LayoutInflater
@@ -14,7 +31,22 @@ import com.shabinder.musicForEveryone.fragments.MainFragment
 import com.shabinder.musicForEveryone.models.Track
 import com.shabinder.musicForEveryone.utils.bindImage
 import kotlinx.coroutines.launch
-
+/*
+* Copyright (C)  2020  Shabinder Singh
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 class TrackListAdapter:RecyclerView.Adapter<TrackListAdapter.ViewHolder>(),DownloadHelper {
 
     var trackList = listOf<Track>()
