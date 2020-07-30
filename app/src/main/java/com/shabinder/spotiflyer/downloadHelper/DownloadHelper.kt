3df41fbe87e538963e949adb838328470cdb0e6e
shabinder <mr.shabinder@outlook.com>
2020-07-30 15:32:48 +0530

    Updated Removal of Illegal char fun.(Bug-Fixed)

--- app/src/main/java/com/shabinder/spotiflyer/downloadHelper/DownloadHelper.kt
+++ app/src/main/java/com/shabinder/spotiflyer/downloadHelper/DownloadHelper.kt
@@ -166,13 +166,20 @@ object DownloadHelper {
             '\"',
             '.',
             ':',
-            '-'
+            '-',
+            '\''
         )
+
         var name = fileName
         for (c in illegalCharArray) {
             name = fileName.replace(c, '_')
         }
         name = name.replace("\\s".toRegex(), "_")
+        name = name.replace("\\)".toRegex(), "")
+        name = name.replace("\\(".toRegex(), "")
+        name = name.replace("\\[".toRegex(), "")
+        name = name.replace("]".toRegex(), "")
+        name = name.replace("\\.".toRegex(), "")
         return name
     }
 }
