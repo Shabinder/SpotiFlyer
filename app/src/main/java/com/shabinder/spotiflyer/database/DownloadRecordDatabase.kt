30558a36e672c6fe485824b21afa14be251c62cc
Shabinder <mr.shabinder@outlook.com>
2020-12-04 14:18:13 +0530

    1.7 crash fixes

--- app/src/main/java/com/shabinder/spotiflyer/database/DownloadRecordDatabase.kt
+++ app/src/main/java/com/shabinder/spotiflyer/database/DownloadRecordDatabase.kt
@@ -22,7 +22,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [DownloadRecord::class], version = 1, exportSchema = false)
+@Database(entities = [DownloadRecord::class], version = 2, exportSchema = false)
 abstract class DownloadRecordDatabase:RoomDatabase() {
 
     abstract val databaseDAO: DatabaseDAO
