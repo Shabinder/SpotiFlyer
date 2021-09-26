9add1dd88cabccd4c12a5c62de2d2730208e20c5
shabinder <owner@shabinder.in>
2021-09-26 13:43:59 +0530

    Improve Special Character Remover

--- common/data-models/src/commonMain/kotlin/com/shabinder/common/utils/Utils.kt
+++ common/data-models/src/commonMain/kotlin/com/shabinder/common/utils/Utils.kt
@@ -1,7 +1,5 @@
 package com.shabinder.common.utils
 
-import io.github.shabinder.TargetPlatforms
-import io.github.shabinder.activePlatform
 import kotlinx.serialization.json.Json
 import kotlin.native.concurrent.ThreadLocal
 
@@ -17,41 +15,5 @@ val globalJson by lazy {
  * Removing Illegal Chars from File Name
  * **/
 fun removeIllegalChars(fileName: String): String {
-    if (activePlatform is TargetPlatforms.Js) return fileName
-    val illegalCharArray = charArrayOf(
-        '/',
-        '\n',
-        '\r',
-        '\t',
-        '\u0000',
-        '\u000C',
-        '`',
-        '?',
-        '*',
-        '\\',
-        '<',
-        '>',
-        '|',
-        '\"',
-        '.',
-        '-',
-        '\''
-    )
-
-    var name = fileName
-    for (c in illegalCharArray) {
-        name = fileName.replace(c, '_')
-    }
-    name = name.replace("\\s".toRegex(), "_")
-    name = name.replace("/".toRegex(), "_")
-    name = name.replace("\\)".toRegex(), "")
-    name = name.replace("\\(".toRegex(), "")
-    name = name.replace("\\[".toRegex(), "")
-    name = name.replace("]".toRegex(), "")
-    name = name.replace("\\.".toRegex(), "")
-    name = name.replace("\"".toRegex(), "")
-    name = name.replace("\'".toRegex(), "")
-    name = name.replace(":".toRegex(), "")
-    name = name.replace("\\|".toRegex(), "")
-    return name
+    return fileName.replace("[^\\dA-Za-z_]".toRegex(), "_")
 }
